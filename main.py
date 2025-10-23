0bcac5a53da6a70b0f4465a3efcc0737e1be501b
kojozcodes <kojozcodes@gmail.com>
2025-10-23 11:48:37 +0100

    get book word count

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def get_book_text(filepath):
         return contents
 
 
-def main():
-    print(get_book_text("books/frankenstein.txt"))
+def get_word_count(book_content):
+    split_words = book_content.split()
+    return len(split_words)
 
+
+def main():
+    book_content = get_book_text("books/frankenstein.txt")
+    word_count = get_word_count(book_content)
+    print(book_content)
+    print(f"Found {word_count} total words")
 
 main()

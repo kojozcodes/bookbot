a23e1394681e9c13c691a76cfcf856b42a8e56f2
kojozcodes <kojozcodes@gmail.com>
2025-10-23 11:52:07 +0100

    add stats.py and refactor code

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+from stats import get_word_count
+
+
 def get_book_text(filepath):
     with open(filepath) as file:
         contents = file.read()
         return contents
-
-
-def get_word_count(book_content):
-    split_words = book_content.split()
-    return len(split_words)
 
 
 def main():
@@ -14,5 +12,6 @@ def main():
     word_count = get_word_count(book_content)
     print(book_content)
     print(f"Found {word_count} total words")
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(book_content):
+    split_words = book_content.split()
+    return len(split_words)

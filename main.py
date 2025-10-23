6f44bbdff88014556cc87508f3c6cda683852714
kojozcodes <kojozcodes@gmail.com>
2025-10-23 11:38:23 +0100

    read book content

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        contents = file.read()
+        return contents
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
